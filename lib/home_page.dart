b0e8712263859ed3e36bd336dcd50c0128700351
chetan1905 <chetanmdixit@gmail.com>
2022-11-23 13:22:54 +0530

    Second day

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatelessWidget {
+  //const HomePage({super.key});
+
+  @override 
+  Widget build(BuildContext context) {
+    const int days = 30;
+    const String name = "Chetan";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+        body: Center(
+          child: Text("Welcome to $days days of flutter with $name"),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
