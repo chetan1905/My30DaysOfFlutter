689f7f98062ff3fa4f5fda086f6e8c7e5068d11c
chetan1905 <chetanmdixit@gmail.com>
2022-11-26 12:12:26 +0530

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -13,17 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
-      routes: {
-        "/":(context) => LoginPage(),
-        "/home":(context) => HomePage()
-      },
+      initialRoute: "/",
+      routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
     );
   }
 }
