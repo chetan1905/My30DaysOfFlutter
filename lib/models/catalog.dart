aae7ac8f4a3a459f8f00009ee06c3c7aecbe68ee
chetan1905 <chetanmdixit@gmail.com>
2022-11-28 13:39:23 +0530

    nineth commit

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+      id: "Chetan001",
+      name: "iphone 12 pro",
+      desc: "Apple iphone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://img2.gadgetsnow.com/gd/images/products/additional/large/G201746_View_1/mobiles/smartphones/apple-iphone-12-pro-128-gb-pacific-blue-6-gb-ram-.jpg"),
+];
